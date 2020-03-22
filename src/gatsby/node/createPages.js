db1f8670acfcb6ee8e43ea666fc32ab95fdc1e04
s.matveykin <s.matveykin@corp.mail.ru>
2020-03-22 20:39:50 +0300

    change yandex turbo

--- src/gatsby/node/createPages.js
+++ src/gatsby/node/createPages.js
@@ -43,14 +43,46 @@ module.exports = async ({ graphql, actions }) => {
             }
           }
   */
-  services.forEach((post, i) => {
+  services.forEach((post, i, array) => {
     const service = post.node
 
     feed.item({
       title: service.title,
       image_url: service.img && service.img.file.url,
       url: `https://autohof24.ru/services/${service.slug}/`,
-      content: fromRichTextToText(service.description.description),
+      content:
+        fromRichTextToText(service.description.description) +
+        `
+        <button
+          formaction="https://autohof24/online"
+          data-background-color="#F17E01"
+          data-color="white"
+          data-turbo="false"
+          data-primary="true"
+          disabled
+        >
+          Записаться на сайте
+        </button>
+        <p>Цены по категориям</p>
+      <table data-invisible="false">
+    <tr>
+        <th>Легковой</th>
+        <th>Страна</th>
+        <th>Микроавтобус</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Внедорожник</td>
+        <td>Микроавтобус</td>
+    </tr>
+    <tr>
+        <td>${service.passenger}</td>
+        <td>${service.offroad}</td>
+        <td>${service.bus}</td>
+    </tr>
+
+</table>
+      `,
       menu: [
         {
           link: 'http://autohof.ru/',
@@ -69,6 +101,13 @@ module.exports = async ({ graphql, actions }) => {
           text: 'О нас',
         },
       ],
+      related: array
+        .filter(item => item.node.id !== service.id)
+        .map(i => ({
+          link: `https://autohof24.ru/services/${i.node.slug}/`,
+          image_url: i.node.img && i.node.img.file.url,
+          text: i.node.title,
+        })),
     })
     const slug = service.slug
     createPage({
