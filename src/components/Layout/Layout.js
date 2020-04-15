7d0b72c86690b1a0c0da0b7a7dc8b291c3e4a898
s.matveykin <s.matveykin@corp.mail.ru>
2020-04-15 13:59:04 +0300

    reload yandex metrics

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,6 @@ import Menu from 'components/Menu'
 import Footer from 'containers/Footer'
 import { globalStyles } from 'styles/globalStyles.js'
 import { useStaticQuery, graphql } from 'gatsby'
-import { YMInitializer } from 'react-yandex-metrika'
 
 const Root = styled.div`
   font-family: ${props => props.theme.fonts.body};
@@ -58,6 +57,25 @@ const Layout = props => {
   return (
     <Root className="siteRoot">
       <div itemscope="true" itemtype="http://schema.org/Service">
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<!-- Yandex.Metrika counter -->
+              <script type="text/javascript" >
+                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                 m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              
+                 ym(61192993, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                 });
+              </script>
+              <noscript><div><img src="https://mc.yandex.ru/watch/61192993" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+              <!-- /Yandex.Metrika counter -->`,
+          }}
+        ></div>
         <meta itemprop="serviceType" content="Автосервис" />
 
         <div className="siteContent">
@@ -66,10 +84,6 @@ const Layout = props => {
         </div>
         <Footer />
         <Global styles={globalStyles} />
-        <YMInitializer
-          options={{ webvisor: true, defer: true }}
-          accounts={[61192993]}
-        />
       </div>
     </Root>
   )
